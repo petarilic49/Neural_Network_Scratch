f050869c6ff6517141534a89755a6cd964c88304
Petar Ilic <petarilic49@gmail.com>
2021-05-20 13:17:30 -0400

    added eigen library

--- main.cpp
+++ main.cpp
@@ -2,11 +2,37 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <Eigen/Dense>
+#include <time.h> // Used to create randomly generated weights between 0 and 1
 #include <sstream> //Used to convert the string values into integer values
 #include <fstream> //Input/output stream class to operate on files
 
 
 using namespace std;
+
+// Create a class that will stores a dense layer
+class neural_layer{
+    public:
+    vector<vector<double>> weights, biases;
+        neural_layer(int num_inputs, int num_neurons){ //Constructor
+            // Need to randomly generate weights for each input to each neuron
+            vector<double> whold;
+            srand( (unsigned)time( NULL ) );
+            for(int i = 0; i<num_neurons; i++){ // Column
+                for(int j = 0; j<num_inputs; j++){ // Row
+                    //cout << "Randon Weight is: " << (double) rand()/RAND_MAX << endl;
+                    whold.push_back((double) rand()/RAND_MAX);
+                }
+                weights.push_back(whold);
+                whold.clear();
+            }
+            return;
+        }
+        void forward(){
+            // Creates forward propogation on the layer
+            return;
+        }
+};
 
 // Function reads the .csv file and stores/returns the data inputs in a 2D vector
 vector<vector<string>> readDataFile(string fileName){
@@ -153,10 +179,25 @@ int main()
     normalizeData(&x_trainingData);
     normalizeData(&x_testingData);
 
-
     
+    // We want weights for each neuron to be randomly selected within the range of -1 and 1, prevents output/data explosion
+    // In terms of biases initialize them as 0, but if you notice that the output is always 0 there may be a 'dead network' which means weights are multiplied by 0
 
-    
+    cout<<"Weights generated for each neuron:"<<endl;
+    /*srand( (unsigned)time( NULL ) );
+ 
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << (double) rand()/RAND_MAX << endl;
+    }*/
+
+    neural_layer hidden_layer(4, 3);
+
+    /*for(int i = 0; i<3; i++){ // Column
+        for(int j = 0; j<4; j++){ // Row
+            cout << hidden_layer.weights[i][j] << endl;
+        }
+    }*/
 
     return 0;
 }
